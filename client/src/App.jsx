429a7cebd6f34dfc0251fd1fe20578cc4a4ded1a
HELOISA RANGEL GUIMARÃES <h.guimaraes@labcetec.net>
2024-10-02 16:59:14 -0300

    Criando barra de navegacao com responsavidade

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,11 @@
 import './App.css';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import rrd from 'react-router-dom';
-
 import Home from './components/pages/Home';
 import Login from './components/pages/Login';
-
+import GestaoUsuario from './components/pages/GestaoUsuario'
+import ImporCSV from './components/pages/ImportCSV'
+import CadastroAula from './components/pages/CadastroAula'
 
 function App() {
   return (
@@ -15,6 +14,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/gestao_usuario' element={<GestaoUsuario/>} />
+        <Route path='/import_csv' element={<ImporCSV/>} />
+        <Route path='/cadastro_aula' element={<CadastroAula/>} />
       </Routes>
     </Router>
   );
